26743d5711e50df0eb7bea7850c02cd83642a760
afdikk <144641714+afdikk@users.noreply.github.com>
2024-12-12 10:37:52 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -2,6 +2,13 @@ import java.util.Scanner;
 
 public class Nomor1 {
 
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int N = input.nextInt();
+        cetakFizzBuzz(N);
+        System.out.println(n1.class.getDeclaredMethods().length); 
+    }
+    
     public static void cetakFizzBuzz(int n) {
         for (int i = 0; i <= n; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
@@ -15,14 +22,4 @@ public class Nomor1 {
             }
         }
     }
-
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        int N = input.nextInt();
-        cetakFizzBuzz(N);
-        System.out.println(n1.class.getDeclaredMethods().length); 
-    }
-    
-    
 }
-
