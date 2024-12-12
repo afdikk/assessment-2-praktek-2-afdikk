522f15cceeecb8e895847a274546d5833e03a21a
afdikk <144641714+afdikk@users.noreply.github.com>
2024-12-12 10:51:11 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -4,12 +4,23 @@ public class Nomor1 {
 
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+        System.out.println("Masukkan nilai N: ");
         int N = input.nextInt();
-        /* Pekerjaan anda mulai dari sini */
-
-        /* Pekerjaan anda berakhir sini */
-
+        cetakFizzBuzz(N);
         System.out.println(Nomor1.class.getDeclaredMethods().length); 
     }
     
+    public static void cetakFizzBuzz(int n) {
+        for (int i = 0; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
 }
